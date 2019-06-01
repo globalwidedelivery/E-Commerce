a70ec4315bc1debfd65ce41b257ccce019a75180
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2019-06-01 19:20:16 +0000

    initial commit

---
+++ application/logs/log-2019-05-31.php
@@ -0,0 +1,205 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-05-31 15:44:30 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'ecommerce' /Volumes/Lux/dev/web/php/ecommerce/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2019-05-31 15:44:30 --> Unable to connect to the database
+ERROR - 2019-05-31 15:46:06 --> Severity: Warning --> mkdir(): Invalid path /Volumes/Lux/dev/web/php/ecommerce/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-05-31 15:46:06 --> Severity: Error --> session_start(): Failed to initialize storage module: user (path: ) /Volumes/Lux/dev/web/php/ecommerce/system/libraries/Session/Session.php 143
+ERROR - 2019-05-31 17:19:34 --> Severity: Warning --> mkdir(): Invalid path /Volumes/Lux/dev/web/php/ecommerce/system/libraries/Session/drivers/Session_files_driver.php 136
+ERROR - 2019-05-31 17:19:34 --> Severity: error --> Exception: Session: Configured save path '' is not a directory, doesn't exist or cannot be created. /Volumes/Lux/dev/web/php/ecommerce/system/libraries/Session/drivers/Session_files_driver.php 138
+ERROR - 2019-05-31 17:29:18 --> Query error: Unknown column 'data' in 'field list' - Invalid query: SELECT `data`
+WHERE `id` = 'qtc3erdad920mjvt1ncd4c87ndh99ius'
+ERROR - 2019-05-31 17:29:18 --> Severity: Warning --> session_write_close(): Cannot call session save handler in a recursive manner Unknown 0
+ERROR - 2019-05-31 17:29:18 --> Severity: Warning --> session_write_close(): Failed to write session data using user defined save handler. (session.save_path: ) Unknown 0
+ERROR - 2019-05-31 17:29:34 --> Query error: Unknown column 'data' in 'field list' - Invalid query: SELECT `data`
+WHERE `id` = 'qtc3erdad920mjvt1ncd4c87ndh99ius'
+ERROR - 2019-05-31 17:29:34 --> Severity: Warning --> session_write_close(): Cannot call session save handler in a recursive manner Unknown 0
+ERROR - 2019-05-31 17:29:34 --> Severity: Warning --> session_write_close(): Failed to write session data using user defined save handler. (session.save_path: ) Unknown 0
+ERROR - 2019-05-31 17:29:37 --> Query error: Unknown column 'data' in 'field list' - Invalid query: SELECT `data`
+WHERE `id` = 'qtc3erdad920mjvt1ncd4c87ndh99ius'
+ERROR - 2019-05-31 17:29:37 --> Severity: Warning --> session_write_close(): Cannot call session save handler in a recursive manner Unknown 0
+ERROR - 2019-05-31 17:29:37 --> Severity: Warning --> session_write_close(): Failed to write session data using user defined save handler. (session.save_path: ) Unknown 0
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:04 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:05 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:07 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:07 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:10 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:11 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:11 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:11 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:11 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:20 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:22 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:22 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:23 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:23 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:24 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:24 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:24 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:25 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:25 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:25 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:25 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:41:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:48:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:48:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:37 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:37 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:37 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:37 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:37 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 17:50:39 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:00 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:13 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:13 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:19 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:38 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:41 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:41 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:41 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:43 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:43 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:49 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:49 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:50 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:50 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:52 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:53 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:55 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:01:55 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:28 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:29 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:29 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:34 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:36 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:06:36 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:20 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:20 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:20 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:49 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:49 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:50 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:07:50 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:41 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:47 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:47 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:53 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:54 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:09:55 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:10:08 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'ASC' at line 1 - Invalid query: SELECT YEAR(FROM_UNIXTIME(date)) as year, MONTH(FROM_UNIXTIME(date)) as month, COUNT(id) as num FROM orders GROUP BY YEAR(FROM_UNIXTIME(date)), MONTH(FROM_UNIXTIME(date)) ASC
+ERROR - 2019-05-31 18:10:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:10:58 --> 404 Page Not Found: ../modules/vendor/controllers//index
+ERROR - 2019-05-31 18:10:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:11:03 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:11:08 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:13:34 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:13:34 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:14:23 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:14:26 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:14:29 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:14:29 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:14:29 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:19:02 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:19:03 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:19:03 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:19:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:19:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:01 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:09 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:47 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:55 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:20:58 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:21:12 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:33:54 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:33:54 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:34:13 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:34:13 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:42:43 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:42:43 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:42:48 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:42:48 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:42:51 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:43:01 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:45:07 --> 404 Page Not Found: /index
+ERROR - 2019-05-31 18:45:12 --> 404 Page Not Found: /index
